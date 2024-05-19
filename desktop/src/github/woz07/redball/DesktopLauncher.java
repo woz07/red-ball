3225fa114c1fba0fb06eb2c2607ae9171a87fc67
woz07 <105782312+woz07@users.noreply.github.com>
2024-05-19 23:31:41 +0100

    Initial commit

---
+++ desktop/src/github/woz07/redball/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package github.woz07.redball;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import github.woz07.redball.Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("red-ball");
+		new Lwjgl3Application(new Game(), config);
+	}
+}
