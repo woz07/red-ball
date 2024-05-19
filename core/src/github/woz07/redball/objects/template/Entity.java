a03c9a4a3625bc74a7d8ae76babdbe809d08c65c
woz07 <105782312+woz07@users.noreply.github.com>
2024-05-20 00:10:49 +0100

    Add: Hp again

--- core/src/github/woz07/redball/objects/template/Entity.java
+++ core/src/github/woz07/redball/objects/template/Entity.java
@@ -15,26 +15,26 @@ import com.badlogic.gdx.math.Vector2;
 public class Entity
 {
     // Base features of an entity
-    private boolean hit;            // If hit then game is over
+    private int hp;                 // The hp of the entity
     private double width, height;   // The dimensions of the entity
     private Vector2 position;       // The x & y of the entity
     private Texture image;          // The image to render of the entity
     
-    public Entity(boolean hit, int width, int height, Vector2 position, Texture image)
+    public Entity(int hp, int width, int height, Vector2 position, Texture image)
     {
-        this.hit       = hit;
+        this.hp       = hp;
         this.width    = width;
         this.height   = height;
         this.position = position;
         this.image    = image;
     }
     
-    public boolean isHit() {
-        return hit;
+    public int getHp() {
+        return hp;
     }
     
-    public void setHit(boolean hit) {
-        this.hit = hit;
+    public void setHit(int hp) {
+        this.hp = hp;
     }
     
     public double getWidth() {
